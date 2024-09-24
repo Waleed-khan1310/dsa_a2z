a3175d836bef742739669f6f96c6cf98e89df5aa
Waleed Khan <waleedkhan13102000@gmail.com>
2024-09-24 14:38:21 +0500

    update

--- OOP/01. Class.py
+++ OOP/01. Class.py
@@ -10,24 +10,3 @@ print(s1.rollnum)
 
 s2=student('Arif',97)
 print(s2.rollnum)
-
-
-# In this we are adding some functionality to our class (full_name)
-class car:
-    def __init__(self,company,model):
-        self.company=company
-        self.model=model
-    
-    def full_name(self):
-        return f"{self.company} {self.model}"
-    
-class electriccar(car):
-    def __init__(self, company, model, batterySize):
-        super().__init__(company, model)
-        self.batterySize=batterySize
-    
-s3=car("Toyota","supra")
-print(s3.model)
-print(s3.full_name())
-my_tesla=electriccar("tesla","model s","1000Kwh")
-print(my_tesla.full_name() , my_tesla.batterySize)
